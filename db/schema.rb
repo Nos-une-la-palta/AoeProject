dbb2b297031fa22d422271e10a364d5ea8182353
MirsonCamadro <kztempus@gmail.com>
2019-11-09 15:39:48 -0300

    added Nick to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_140618) do
+ActiveRecord::Schema.define(version: 2019_11_09_183803) do
 
   create_table "forum_categories", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_140618) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nick"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
