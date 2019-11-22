772e1c640ef3a0560e4a04443777e0fea0bb4b75
MirsonCamadro <kztempus@gmail.com>
2019-11-22 17:12:09 -0300

    moving forumcategory.create from db:create to db:seed

--- db/migrate/20191108140615_create_forum_categories.simple_discussion.rb
+++ db/migrate/20191108140615_create_forum_categories.simple_discussion.rb
@@ -8,42 +8,5 @@ class CreateForumCategories < ActiveRecord::Migration[4.2]
 
       t.timestamps
     end
-
-    ForumCategory.reset_column_information
-
-    ForumCategory.create(
-      name: "General",
-      color: "#FF4754",
-    )
-
-    ForumCategory.create(
-      name: 'Estrategias',
-      color: '#6041E8'
-  )
-  
-  ForumCategory.create(
-      name: 'Torneos',
-      color: '#54FFEE'
-  )
-  
-  ForumCategory.create(
-      name: 'Downloads',
-      color: '#A2E841'
-  )
-  
-  ForumCategory.create(
-      name: 'Torneos antiguos',
-      color: 'FFB840'
-  )
-  
-  ForumCategory.create(
-      name: 'Vods',
-      color: '#FF8D47'
-  )
-  ForumCategory.create(
-      name: 'Partidas grabadas',
-      color: '#c8ed32'
-  )
-
   end
 end
