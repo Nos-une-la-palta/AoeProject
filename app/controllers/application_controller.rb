dd34b01bb942ddecc6eba4a510f9e691d8565ca6
MirsonCamadro <kztempus@gmail.com>
2019-11-22 17:25:20 -0300

    added strong params :nick and :image to update user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_account_update_params, if: :devise_controller?
   
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:image, :nick, :moderator])
     end
+
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:image, :nick])
+    end
+
   end
