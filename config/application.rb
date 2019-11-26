348f14afa627dd4acfcda5e577502433959d7efd
MirsonCamadro <kztempus@gmail.com>
2019-11-26 16:23:03 -0300

    try gem rack-cors

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,13 @@ module AoeProject
     # the framework and any gems in your application.
   end
 end
+
+class Application < Rails::Application
+  # Access-Control-Allow-Origin
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://radiant-bastion-31318.herokuapp.com', /https*:\/\/.*?bloopist\.com/
+      resource '*', :headers => :any, :methods => :any
+    end
+  end
+end
