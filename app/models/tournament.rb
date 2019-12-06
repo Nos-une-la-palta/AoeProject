30199c7b7c66fe0c20f5a060826b802e4d36369d
MirsonCamadro <kztempus@gmail.com>
2019-12-06 15:50:13 -0300

    added in torunament if value = 0 then gratis! if price = 0 then honor!

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,12 @@
 class Tournament < ApplicationRecord
     validates :title, :map, :content, presence: true
     validates :title, length: { maximum: 50 }
+
+    def free?
+      value == 0
+    end
+
+    def no_price?
+      price == 0
+    end
 end
